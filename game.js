95bd593f695a1da297af488740b78f2fe1d16374
jeffsimp88 <JeffreySimpsonKenzie>
2021-10-14 11:33:38 -0400

    Added button for another pokemon

--- game.js
+++ game.js
@@ -1,29 +1,40 @@
 const baseURL = "https://pokeapi.co/api/v2";
 const gameElement = document.querySelector(".whosThatPokemon");
-const pokemonImgBox = document.querySelector(".pokemonImg");
 const getRandomId = (maxId) => Math.floor(Math.random() * maxId + 1);
 
 function fetchPokemon() {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${getRandomId(386)}`)
+  let getId = getRandomId(386);
+  fetch(`https://pokeapi.co/api/v2/pokemon/${getId}`)
     .then((response) => response.json())
     .then(function (pokemon) {
-      let pokeImg = document.createElement("img");
-      pokeImg.classList.add("pokemon-sprite", "pokemon-sprite-silhouette");
-      pokeImg.src = pokemon.sprites.front_default;
-
-      let pokemonName = document.createElement("h3");
-      pokemonName.append(
-        `It's ${pokemon.name[0].toUpperCase()}${pokemon.name.slice(1)}!`
-      );
-      pokemonName.classList.add("hidden");
-
-      pokeImg.addEventListener("click", () => {
-        pokeImg.classList.toggle("pokemon-sprite-silhouette");
-        pokemonName.classList.toggle("hidden");
-      });
-      pokemonImgBox.append(pokeImg);
-      gameElement.append(pokemonName);
+      createGuessElement(pokemon);
     });
 }
 
+function createGuessElement(pokemon) {
+  gameElement.innerHTML = "";
+  let pokemonImgBox = document.createElement("div");
+  pokemonImgBox.classList.add("pokemonImg");
+  let pokeImg = document.createElement("img");
+  pokeImg.classList.add("pokemon-sprite", "pokemon-sprite-silhouette");
+  pokeImg.src = pokemon.sprites.front_default;
+
+  let pokemonName = document.createElement("h3");
+  pokemonName.append(
+    `It's ${pokemon.name[0].toUpperCase()}${pokemon.name.slice(1)}!`
+  );
+  pokemonName.classList.add("hidden");
+
+  pokeImg.addEventListener("click", () => {
+    pokeImg.classList.toggle("pokemon-sprite-silhouette");
+    pokemonName.classList.toggle("hidden");
+  });
+  pokemonImgBox.append(pokeImg);
+  gameElement.append(pokemonImgBox, pokemonName);
+}
+
 fetchPokemon();
+
+document
+  .querySelector(".pokemonButton.getPokemon")
+  .addEventListener("click", fetchPokemon);
